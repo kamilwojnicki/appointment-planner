53e27c5a1d782e8e3316c8fab3017edc41986c3d
Kamil <kamil@runspiration.pl>
2022-05-08 11:49:30 +0200

    deletee bugs

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -5,7 +5,7 @@ const values = Object.values(tile);
   return (
     <div className="tile-container">
       {values.map((el, index) => (
-        <p className={index == 0 ? "tile-title" : "tile"} >{el}</p>
+        <p className={index === 0 ? "tile-title" : "tile"} >{el}</p>
       ))}
     </div>
   );
